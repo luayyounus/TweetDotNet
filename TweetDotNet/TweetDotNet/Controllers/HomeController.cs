e844496970070ec00e6d71200484d17f927ed5b5
Luay <luayghazwan@yahoo.com>
2018-02-07 01:05:25 -0800

    Home controller comments addition

--- TweetDotNet/TweetDotNet/Controllers/HomeController.cs
+++ TweetDotNet/TweetDotNet/Controllers/HomeController.cs
@@ -8,13 +8,16 @@ using TweetDotNet.Models;
 
 namespace TweetDotNet.Controllers
 {
+    // main home controller provided by Asp.NET MVC that inherits from Controller
     public class HomeController : Controller
     {
+        // Default index action that returns the index view
         public IActionResult Index()
         {
             return View();
         }
 
+        // Default about action that return About view
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
@@ -22,6 +25,7 @@ namespace TweetDotNet.Controllers
             return View();
         }
 
+        // Default contact me action with its view 
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
@@ -29,6 +33,7 @@ namespace TweetDotNet.Controllers
             return View();
         }
 
+        // Error action returning the error view model with requested id or the unique id from logs
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
