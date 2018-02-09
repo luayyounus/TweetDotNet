7db43d34ed288193b23c30abdbcc463ea9958cbf
Luay <luayghazwan@yahoo.com>
2018-02-08 23:48:51 -0800

    Tweet Blog model created

---
+++ TweetDotNet/TweetDotNet/Models/TweetBlog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetDotNet.Models
+{
+    public class TweetBlog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string HeadLine { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Content { get; set; }
+    }
+}
