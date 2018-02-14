b712b53ea973a0144aa29e2fc36c593fdeb90283
Luay <luayghazwan@yahoo.com>
2018-02-14 12:50:12 -0800

    Tweet Blog

--- TweetDotNet/TweetDotNet/Models/TweetBlog.cs
+++ TweetDotNet/TweetDotNet/Models/TweetBlog.cs
@@ -8,7 +8,7 @@ namespace TweetDotNet.Models
     public class TweetBlog
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string HeadLine { get; set; }
         public DateTime DateCreated { get; set; }
         public string Content { get; set; }
