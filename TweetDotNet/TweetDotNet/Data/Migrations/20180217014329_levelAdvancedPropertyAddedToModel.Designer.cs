229477fec4e6ee2ce7e92fd9df029c25a6ee65cd
Luay <luayghazwan@yahoo.com>
2018-02-16 17:46:27 -0800

    modified to fix the view components

--- TweetDotNet/TweetDotNet/Data/Migrations/20180217014329_levelAdvancedPropertyAddedToModel.Designer.cs
+++ TweetDotNet/TweetDotNet/Data/Migrations/20180217014329_levelAdvancedPropertyAddedToModel.Designer.cs
@@ -11,9 +11,10 @@ using TweetDotNet.Data;
 namespace TweetDotNet.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180217014329_levelAdvancedPropertyAddedToModel")]
+    partial class levelAdvancedPropertyAddedToModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
